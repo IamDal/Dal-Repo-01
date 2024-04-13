ca46785dc6571225840efc9b1d4c3f2a3199a127
Dalton Burton <115256541+IamDal@users.noreply.github.com>
2024-04-13 16:57:06 +0000

    build docker and requirements file

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
-DATA_PATH = './src/data/Churn_Modelling.csv'
-PROCESSED_DATA_PATH = './src/data/cleaned_data.csv'
+DATA_PATH = './data/Churn_Modelling.csv'
+PROCESSED_DATA_PATH = './data/cleaned_data.csv'
 COLUMNS_TO_REMOVE = ['CustomerId','Surname']
 NUMERICAL_COLUMNS = ['Gen_Prod', 'CreditScore', 'Balance', 'NumOfProducts', 'HasCrCard',
        'Tenure', 'IsActiveMember', 'Active__Prod', 'Age', 'EstimatedSalary',
        'Generation', 'Gen_Geo', 'Surname_encoded']
 CATEGORICAL_COLUMNS = ['GeoGender', 'Geography', 'Gender', 'HasBalance']
-SAVE_MODEL = './src/model/model.pkl'
+SAVE_MODEL = './model/model.pkl'
